202b1c50613fc5a757341559b1b89d7e8bff70d8
akeddy <alex.keddy.bcs@gmail.com>
2022-08-31 10:11:34 -0300

    renamed cache copy

--- increment/lib/increment/jobs/commit_increment.ex
+++ increment/lib/increment/jobs/commit_increment.ex
@@ -1,6 +1,8 @@
 defmodule Increment.CommitIncrement do
   use GenServer
 
+  @schedule Application.get_env(:increment, :persist_cache, 30 * 1000)
+
   def start_link(_opts) do
     GenServer.start_link(__MODULE__, %{})
   end
@@ -13,13 +15,15 @@ defmodule Increment.CommitIncrement do
   def handle_info(:work, state) do
     # Do the work you desire here
     query = Cachex.Query.create(true, {:key, :value})
-    cache = :persistence_cache
+    temp_cache = :persistence_cache
             |> Cachex.stream!(query)
             |> Enum.map(fn {k,v} -> %{"key" => k, "value" => v} end)
     Cachex.reset(:persistence_cache)
 
+    # cant decide if this is a great idea or not yet
+    # the transaction protects what's going in, but if we've got millions of unique keys we become much more at risk of the psql connection timing out
     Increment.Repo.transaction(fn ->
-      Enum.map(cache, fn c ->
+      Enum.map(temp_cache, fn c ->
         Increment.Tasks.create_counter(c)
       end)
     end)
@@ -29,6 +33,6 @@ defmodule Increment.CommitIncrement do
   end
 
   defp schedule_work() do
-    Process.send_after(self(), :work, 1 * 30 * 1000) # In 5 minutes
+    Process.send_after(self(), :work, @schedule)
   end
 end
