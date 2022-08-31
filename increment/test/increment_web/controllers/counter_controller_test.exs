fdff5d65813ec43f43803cc2e0537e9bde917e68
akeddy <alex.keddy.bcs@gmail.com>
2022-08-31 09:40:59 -0300

    - added testing to controller - added some flags to only execute big tests when I think they'll work - need to look into adding sleeps now that I'm using the cache

--- increment/test/increment_web/controllers/counter_controller_test.exs
+++ increment/test/increment_web/controllers/counter_controller_test.exs
@@ -9,75 +9,158 @@ defmodule IncrementWeb.CounterControllerTest do
     key: "some key",
     value: 42
   }
-  @update_attrs %{
-    key: "some updated key",
-    value: 43
-  }
   @invalid_attrs %{key: nil, value: nil}
+
+  @missing_attrs %{}
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
   describe "index" do
-    test "lists all counters", %{conn: conn} do
-      conn = get(conn, Routes.counter_path(conn, :index))
-      assert json_response(conn, 200)["data"] == []
+    test "raise error on disabled common path", %{conn: conn} do
+      assert_raise ArgumentError, fn -> get(conn, Routes.counter_path(conn, :index)) end
     end
   end
 
   describe "create counter" do
     test "renders counter when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.counter_path(conn, :create), counter: @create_attrs)
-      assert %{"id" => id} = json_response(conn, 201)["data"]
+      conn = post(conn, Routes.counter_path(conn, :create), @create_attrs)
+      assert "" = response(conn, 202)
+      counter = Increment.Repo.get_by(Counter, key: @create_attrs.key)
 
-      conn = get(conn, Routes.counter_path(conn, :show, id))
+      # :timer.sleep(1000 * 31) # for cache commits
 
-      assert %{
-               "id" => ^id,
-               "key" => "some key",
-               "value" => 42
-             } = json_response(conn, 200)["data"]
+      assert %Increment.Tasks.Counter{
+        key: "some key",
+        value: 42
+      } = counter
+    end
+
+    test "update counter 100 times", %{conn: conn} do
+      cap = 100
+      range = 1..cap
+      total = 42 * cap
+      conn = 
+        Enum.reduce(range, conn, fn _x, acc ->
+          post(conn, Routes.counter_path(acc, :create), @create_attrs)
+        end)
+      assert "" = response(conn, 202)
+      counter = Increment.Repo.get_by(Counter, key: @create_attrs.key)
+
+      # :timer.sleep(1000 * 31) # for cache commits
+
+      assert %Increment.Tasks.Counter{
+        key: "some key",
+        value: ^total
+      } = counter
+    end
+
+    test "update counter 1000 times", %{conn: conn} do
+      cap = 1000
+      range = 1..cap
+      total = 42 * cap
+      conn = 
+        Enum.reduce(range, conn, fn _x, acc ->
+          post(conn, Routes.counter_path(acc, :create), @create_attrs)
+        end)
+      assert "" = response(conn, 202)
+      counter = Increment.Repo.get_by(Counter, key: @create_attrs.key)
+
+      # :timer.sleep(1000 * 31) # for cache commits
+
+      assert %Increment.Tasks.Counter{
+        key: "some key",
+        value: ^total
+      } = counter
+    end
+
+    test "update counter 10000 times", %{conn: conn} do
+      cap = 10000
+      range = 1..cap
+      total = 42 * cap
+      conn = 
+        Enum.reduce(range, conn, fn _x, acc ->
+          post(conn, Routes.counter_path(acc, :create), @create_attrs)
+        end)
+      assert "" = response(conn, 202)
+      counter = Increment.Repo.get_by(Counter, key: @create_attrs.key)
+
+      # :timer.sleep(1000 * 31) # for cache commits
+
+      assert %Increment.Tasks.Counter{
+        key: "some key",
+        value: ^total
+      } = counter
+    end
+    
+    @tag timeout: 600000
+    @tag :big_test
+    test "update counter 100000 times", %{conn: conn} do
+      cap = 100000
+      range = 1..cap
+      total = 42 * cap
+      conn = 
+        Enum.reduce(range, conn, fn _x, acc ->
+          post(conn, Routes.counter_path(acc, :create), @create_attrs)
+        end)
+      assert "" = response(conn, 202)
+      counter = Increment.Repo.get_by(Counter, key: @create_attrs.key)
+
+      # :timer.sleep(1000 * 31) # for cache commits
+
+      assert %Increment.Tasks.Counter{
+        key: "some key",
+        value: ^total
+      } = counter
+    end
+
+    @tag timeout: 600000
+    @tag :big_test
+    test "update counter 1000000 times", %{conn: conn} do
+      cap = 1000000
+      range = 1..cap
+      total = 42 * cap
+      conn = 
+        Enum.reduce(range, conn, fn _x, acc ->
+          post(conn, Routes.counter_path(acc, :create), @create_attrs)
+        end)
+      assert "" = response(conn, 202)
+      counter = Increment.Repo.get_by(Counter, key: @create_attrs.key)
+
+      # :timer.sleep(1000 * 31 * 5) # for cache commits
+
+      assert %Increment.Tasks.Counter{
+        key: "some key",
+        value: ^total
+      } = counter
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
       conn = post(conn, Routes.counter_path(conn, :create), counter: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
+      assert response(conn, 422) != %{}
     end
+
+    test "renders errors when no data", %{conn: conn} do
+      conn = post(conn, Routes.counter_path(conn, :create), counter: @missing_attrs)
+      assert response(conn, 422) != %{}
+    end
+
   end
 
   describe "update counter" do
     setup [:create_counter]
 
-    test "renders counter when data is valid", %{conn: conn, counter: %Counter{id: id} = counter} do
-      conn = put(conn, Routes.counter_path(conn, :update, counter), counter: @update_attrs)
-      assert %{"id" => ^id} = json_response(conn, 200)["data"]
-
-      conn = get(conn, Routes.counter_path(conn, :show, id))
-
-      assert %{
-               "id" => ^id,
-               "key" => "some updated key",
-               "value" => 43
-             } = json_response(conn, 200)["data"]
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, counter: counter} do
-      conn = put(conn, Routes.counter_path(conn, :update, counter), counter: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
+    test "raise error for invalid common path", %{conn: conn, counter: counter} do
+      assert_raise ArgumentError, fn -> put(conn, Routes.counter_path(conn, :update, counter), counter: @invalid_attrs) end
     end
   end
 
   describe "delete counter" do
     setup [:create_counter]
 
-    test "deletes chosen counter", %{conn: conn, counter: counter} do
-      conn = delete(conn, Routes.counter_path(conn, :delete, counter))
-      assert response(conn, 204)
-
-      assert_error_sent 404, fn ->
-        get(conn, Routes.counter_path(conn, :show, counter))
-      end
+    test "raise error for invalid common path", %{conn: conn, counter: counter} do
+      assert_raise ArgumentError, fn -> delete(conn, Routes.counter_path(conn, :delete, counter)) end
     end
   end
 
--- increment/test/test_helper.exs
+++ increment/test/test_helper.exs
@@ -1,2 +1,4 @@
 ExUnit.start()
+ExUnit.configure(exclude: [:big_test])
 Ecto.Adapters.SQL.Sandbox.mode(Increment.Repo, :manual)
+
